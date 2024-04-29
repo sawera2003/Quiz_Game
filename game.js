cb710dc6dcccecbc6f4316ef03c53dcaa1022282
Sawera Rehman <sawerarehman2003@gmail.com>
2024-04-29 22:42:09 +0500

    just updated

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -73,6 +75,7 @@ let questions = [
 
  getNewQuestion = () => {
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -95,11 +98,21 @@ let questions = [
         
         const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        selectedChoice.parentElement.classList.add(classToApply);
-        // selectedChoice.parentElement.classList.remove(classToApply);
+        if(classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
 
+        selectedChoice.parentElement.classList.add(classToApply);
+setTimeout( () => {
+        selectedChoice.parentElement.classList.remove(classToApply);
         getNewQuestion();
+    }, 1000);
+        
     });
  });
 
+ incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+ };
  startGame();
