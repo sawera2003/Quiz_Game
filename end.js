c982d9ac50b079c6cfaab0264efdb9d604cd09e4
Sawera Rehman <sawerarehman2003@gmail.com>
2024-05-03 22:12:18 +0500

    Update end.js

--- end.js
+++ end.js
@@ -3,7 +3,11 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup" , () => {
     console.log(username.value);
     saveScoreBtn.disabled = !username.value;
@@ -12,4 +16,15 @@ username.addEventListener("keyup" , () => {
 saveHighScore = e => {
   console.log("Click the save button!");
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore;
+    name: username.value
+  };
+  highScores.push(score);
+
+  highScores.sort( (a,b) => {
+    return b.score - a.score;
+  })
+  console.log(highScores)
 };
